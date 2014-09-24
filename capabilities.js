1492485604d3224d35a0d7852f70a71f165bf24c
at1as <jason@willems.ca>
2014-09-24 13:00:25 -0400

    Return single session based on index

--- capabilities.js
+++ capabilities.js
@@ -3,19 +3,20 @@
 
 var conf = require('./conf.json');
 
-var sessions = [];
+var session = [];
 
 if (conf['local']) {
-  sessions = conf.local_settings;
+  session = conf.local_settings[conf.local_index];
 } else {
-  //var r_sessions = conf.remote_settings;
-
+  session = conf.remote_settings[conf.remote_index];
+  session["browserstack.user"]  = process.env.BS_NAME || "";
+  session["browserstack.key"]   = process.env.BS_KEY || "";
+  /*
   conf.remote_settings.forEach(function(session) {
-    //var session = r_session;
     session["browserstack.user"]  = process.env.BS_NAME || "";
     session["browserstack.key"]   = process.env.BS_KEY || "";
     sessions.push(session);
-  });
+  });*/
 }
 
-module.exports = sessions;
+module.exports = session;
