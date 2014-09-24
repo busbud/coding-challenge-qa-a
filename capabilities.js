b2f9c123a72bb4d67583ab30ec64dc6270a53a0b
at1as <jason@willems.ca>
2014-09-23 20:56:56 -0400

    Storing schedule URL

--- capabilities.js
+++ capabilities.js
@@ -1,23 +1,21 @@
-#!/usr/bin/env node
-
 // Select appropriate capabilities based on configuration settings
 // Inject environment variables used for remote sessions
 
 var conf = require('./conf.json');
 
+var sessions = [];
 
 if (conf['local']) {
-  var sessions = conf['local_settings'];
+  sessions = conf.local_settings;
 } else {
-  var sessions = [];
-  var r_sessions = conf['remote_settings'];
+  //var r_sessions = conf.remote_settings;
 
-  for(i=0; i< r_sessions.length; i++) {
-    var session = r_sessions[i];
-    session["browserstack.user"] = process.env.BS_NAME || "";
-    session["browserstack.key"] = process.env.BS_KEY || "";
+  conf.remote_settings.forEach(function(session) {
+    //var session = r_session;
+    session["browserstack.user"]  = process.env.BS_NAME || "";
+    session["browserstack.key"]   = process.env.BS_KEY || "";
     sessions.push(session);
-  }
+  });
 }
 
 module.exports = sessions;
