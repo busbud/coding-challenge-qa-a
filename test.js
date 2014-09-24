5286e5fb5c5ce0871183ef9abc9622bfcf03ba53
at1as <jason@willems.ca>
2014-09-23 23:48:59 -0400

    Moving test.js to top-level directory

--- test.js
+++ test.js
@@ -4,10 +4,10 @@ var fs        = require('fs');
 var async     = require('async');
 var colors    = require('colors');
 
-var sessions  = require(__dirname + '/../../capabilities.js');
-var webdriver = require(__dirname + '/../../driver.js');
-var conf      = require(__dirname + '/../../conf.json');
-var locations = require(__dirname + '/../../locations.json');
+var sessions  = require(__dirname + '/capabilities.js');
+var webdriver = require(__dirname + '/driver.js');
+var conf      = require(__dirname + '/conf.json');
+var locations = require(__dirname + '/locations.json');
 
 
 // Imported server config
@@ -17,7 +17,7 @@ var SCHEDULES_URL = conf.schedule_url;
 
 // Timeouts
 var START_TIMEOUT = 20000;
-var RUN_TIMEOUT   = 15000;
+var TEST_TIMEOUT  = 15000;
 var STOP_TIMEOUT  = 5000;
 
 // Schedules, Date & Time
@@ -27,39 +27,41 @@ var month   = d.getMonth() + 1;
 var day     = d.getDate();
 var MONTH   = [year, month].join('-');
 var TODAY   = [year, month, day].join('-')
+var TMRW    = [year, month, day + 1].join('-')
 
 // Common Functions
 function strEndsWith(str, suffix) {
   return str.toLowerCase().slice(-suffix.toLowerCase().length) === suffix.toLowerCase();
 }
-
 function callback(stream) {
   console.log("Exiting.")
 }
 
 
-// Ensure Kill webdriver on Force Quit (Mac/Linux Only)
+// Kill webdriver on force quit (*NIX Only)
 process.on('SIGINT', function(callback) {
   console.error('\nCaught Unexpected Interrupt Signal. Force Quitting...\n'.red);
   if (driver) { driver.quit() }
   process.exit();
 });
 
-// Catch exceptions from SE and attempt a screenshot where possible
+// Catch exceptions and attempt a screenshot
 process.on('uncaughtException', function(err, callback) {
   console.error('\n\tError... '.red + err);
 
   if (driver) {
     driver.takeScreenshot().then(function(screenshot) {
-      var filepath  = './error/screens/';
-      var filename  = 'suite-setup-' + (new Date).toISOString() + '.png';
+      var browser   = sessions[0].browserName || "";
+      var version   = sessions[0].browser_version || "";
+      var filepath  = './error/screens/' + browser + '/';
+      var filename  = browser + version + (new Date).toISOString() + '.png';
       fs.writeFileSync(filepath + filename, new Buffer(screenshot, 'base64'));
     });
   }
 });
 
 
-// Tests
+// Test Cases
 describe('Routes', function() {
 
   before(function(done) {
@@ -77,16 +79,14 @@ describe('Routes', function() {
     }
     driver.get(host).then(function() {
       console.log('Configuring Test Environment. Navigating to: ' + host.yellow +
-                      '\n' + "Using the following settings: %j" + sessions[0]);
+                    '\n' + 'Using the following settings: ' + '%j'.yellow, sessions[0]);
     }).then(done);
   });
 
-
-
   it('should not contain trip durations in complicated fractions of hours', function(done) {
-    this.timeout(RUN_TIMEOUT);
+    this.timeout(TEST_TIMEOUT);
 
-    var TRIP = SCHEDULES_URL + locations["TOR"] + '/' + locations["MTL"]
+    var TRIP = SCHEDULES_URL + locations["TOR"] + '/' + locations["MTL"];
 
     driver.get(TRIP);
     driver.findElements(webdriver.By.className('duration')).then(function(durations) {
@@ -98,7 +98,8 @@ describe('Routes', function() {
           var dot_location = duration_text.indexOf('.');
           var next_character = duration_text.substring(dot_location + 1, dot_location + 2);
           if (dot_location !== -1) {
-            assert.equal(next_character, "5", '"' + duration_text + '"' + ' contained a fraction that 60 minutes is not divisible by');
+            assert.equal(next_character, "5", '"' + duration_text + '"' +
+                          ' contained a fraction that 60 minutes is not divisible by');
           }
           step();
         });
@@ -107,9 +108,8 @@ describe('Routes', function() {
   });
 
   it('should not render destination names with unexpected characters', function(done) {
-    this.timeout(RUN_TIMEOUT);
+    this.timeout(TEST_TIMEOUT);
 
-    // TODO: run this test for a few consecutive days: today, tomorrow, etc (wrap everything in a forEach???)
     var TRIP = SCHEDULES_URL + locations['PRA'] + '/' + locations['MUN'] + '#date=';
 
     driver.get(TRIP + TODAY);
@@ -120,7 +120,8 @@ describe('Routes', function() {
           return loc.getAttribute('innerHTML');
         }).then(function(location_text) {
           var underscore_location = location_text.indexOf('_');
-          assert.equal(underscore_location, -1, '"' + location_text + '"' + ' had one or more unexpected _ characters');
+          assert.equal(underscore_location, -1, '"' + location_text + '"' +
+                          ' had one or more unexpected _ characters on ' + day);
           step();
         });
       });
@@ -128,7 +129,7 @@ describe('Routes', function() {
   });
 
   it('destinations should not end with a period', function(done) {
-    this.timeout(RUN_TIMEOUT);
+    this.timeout(TEST_TIMEOUT);
 
     var TRIP = SCHEDULES_URL + locations['WASH'] + '/' + locations['NYC'] + '#date=';
 
@@ -142,7 +143,8 @@ describe('Routes', function() {
           var loc_length = location_text.length;
           var last_character = location_text.substring(loc_length - 1);
           if (!(strEndsWith(location_text, ' st.') || (strEndsWith(location_text, ' ave.')))) {
-            assert.equal(last_character, '.', '"' + location_text + '"' + ' ended in an unexpected period');
+            assert.equal(last_character, '.', '"' + location_text + '"' +
+                              ' ended in an unexpected period');
           }
           step();
         });
