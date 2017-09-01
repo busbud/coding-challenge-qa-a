7077c02c29b90ba3d2cdf60b167965848b64b05f
verotremblay <tremblayveronique@gmail.com>
2017-09-01 16:26:27 -0400

    Initial setup + first test

---
+++ selenium-config.js
@@ -0,0 +1,9 @@
+module.exports = {
+ drivers: {
+   chrome: {
+     version: '2.31',
+     arch: process.arch,
+     baseURL: 'https://chromedriver.storage.googleapis.com'
+   },
+ },
+};
---
+++ tests/instagram-link.js
@@ -0,0 +1,21 @@
+const assert = require('assert');
+
+describe('When clicking on the Instagram link', function() {
+	it('should bring you to the Busbud instagram page', function() {
+		// test steps go here
+		browser.url('http://www.busbud.com');
+		browser.click('a.social.instagram');
+
+		browser.pause(3000);
+
+		const lastTabId = browser.getTabIds()[browser.getTabIds().length - 1];
+		browser.switchTab(lastTabId);
+
+		assert(browser.getUrl() === 'https://www.instagram.com/busbudapp/');
+	});
+
+	it('should not be unavailable', function() {
+		console.log(browser.getTitle())
+		assert(browser.getTitle().indexOf('Page Not Found') === -1);
+	});
+});
